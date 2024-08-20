fbc5531ca012b28adffe51c132d37b84055ebc8f
Neelesh Sethi <neeleshsethi@gmail.com>
2024-08-20 10:06:58 +0530

    Refactor: Change OSS index creation

--- assets/lambda-index/create_oss_index.py
+++ assets/lambda-index/create_oss_index.py
@@ -1,10 +1,22 @@
-from requests import request
+#from requests import request
 import json
 import os
 import boto3
 from botocore.auth import SigV4Auth
 from botocore.awsrequest import AWSRequest
 from time import sleep
+from retrying import retry
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+
+boto3_session = boto3.Session()
+region_name = boto3_session.region_name
+sts_client = boto3.client('sts')
+account_id = sts_client.get_caller_identity()["Account"]
+credentials = boto3_session.get_credentials()
+# opensearch service
+service = 'aoss'
+awsauth = auth = AWSV4SignerAuth(credentials, region_name, service)
+
 
 def handler(event, context):
     # 1. Defining the request body for the index and field creation
@@ -12,69 +24,57 @@ def handler(event, context):
     print(f"Collection Endpoint: " + host)
     index_name = os.environ["INDEX_NAME"]
     print(f"Index name: " + index_name)
-    url = host + "/" + index_name
-    print(f"URL: " + url)
-    headers = {
-        'content-type': 'application/json', 
-        'accept': 'application/json',
-    }
-    payload = {
-        "settings": {
-            "index": {
-            "knn": "true"
-            }
-        },
-        "mappings": {
-            "properties": {
-                "vectorField": {
-                    "type": "knn_vector",
-                    "dimension": 1536,
-                    "method": {
-                        "name": "hnsw",
-                        "engine": "faiss",
-                        "space_type": "l2",
-                        "parameters": {
-                            "ef_construction": 1536,
-                            "m": 16,
-                            "ef_search": 1536
+
+
+    body_json = {
+                "settings": {
+                    "index.knn": "true"
+                },
+                "mappings": {
+                    "properties": {
+                        "vector": {
+                            "type": "knn_vector",
+                            "dimension": 1536,
+                            "method": {
+                                "name": "hnsw",
+                                "space_type": "innerproduct",
+                                "engine": "faiss",
+                                "parameters": {
+                                "ef_construction": 256,
+                                "m": 48
+                                }
+                            }
+                        },
+                        "text": {
+                            "type": "text"
+                        },
+                        "text-metadata": {
+                            "type": "text"         
                         }
                     }
-                },
-                "metadataField": {
-                    "type": "text"
-                },
-                "textField": {
-                    "type": "text"
                 }
-            }
-        }
-    }
-    
-    # 2. Obtaining AWS credentials and signing the AWS API request
-    region = os.environ["REGION"]
-    service = 'aoss'
-    credentials = boto3.Session().get_credentials()
-    
-    params = None
-    payload_json = json.dumps(payload)
-    
-    signer = SigV4Auth(credentials, service, region)
+                }
+  
+  
     while True:
         try:        
-            req = AWSRequest(method='PUT', url=url, data=payload_json, params=params, headers=headers)
-            req.headers['X-Amz-Content-SHA256'] = signer.payload(req) # Add the payload hash to the headers as aoss requires it !
-            SigV4Auth(credentials, service, region).add_auth(req)
-            req = req.prepare()
+        
+            oss_client = OpenSearch(
+                        hosts=[{'host': host, 'port': 443}],
+                        http_auth=awsauth,
+                        use_ssl=True,
+                        verify_certs=True,
+                        connection_class=RequestsHttpConnection,
+                        timeout=300
+                    )
+# # It can take up to a minute for data access rules to be enforced
+            sleep(100)
+            response = oss_client.indices.create(index=index_name, body=json.dumps(body_json))
+            print('\nCreating index:')
+            print(response)
 
-            response = request(
-                method=req.method,
-                url=req.url,
-                headers=req.headers,
-                data=req.body
-            )
 
-            if response.status_code != 200:
-                raise Exception(f"Failed to create AOSS index - status: {response.status_code}")
+            
         
         except Exception as e:
             print(f"Retrying to create aoss index...{e}")
